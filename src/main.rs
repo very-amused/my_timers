004d70b1da649d37527055c48da7053fd94f4e1a
very-amused <very-amused@pm.me>
2022-12-20 13:48:20 -0500

    Safe shutdowns

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{env, error::Error, time::Duration};
 use chrono::{Timelike, Local};
-use tokio::{time::{self, sleep}, task::JoinSet};
-use tracing::{event, Level, span, Instrument};
+use tokio::{time::{self, sleep}, task::JoinSet, signal};
+use tracing::{event, Level, span, Instrument, instrument};
 
 mod config;
 mod db;
@@ -9,22 +9,6 @@ mod logging;
 mod events;
 mod cron;
 
-fn to_next_minute() -> Duration {
-	let mut now = chrono::Utc::now();
-	// Ceil to nearest ms + 1
-	// TODO: explain timing of event loop start
-	now = now.with_nanosecond(1_000_000 * (now.nanosecond() / 1_000_000) + 2_000_000).unwrap();
-	let mut next = now.clone();
-	next = if next.minute() == 59 {
-		next.with_hour(next.hour()+1).unwrap()
-		.with_minute(0).unwrap()
-	} else {
-		next.with_minute(next.minute()+1).unwrap()
-	};
-	next = next.with_second(0).unwrap()
-		.with_nanosecond(0).unwrap();
-	(next - now).to_std().unwrap()
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -60,6 +44,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
 		}.instrument(span).await?;
 	}
 
+	// Listen for ctrl+c/SIGINT to safely shutdown.
+	// tokio::select! must be used to catch sigints for all future awaits
+	// on the main threads
+	let ctrl_c = signal::ctrl_c();
+	tokio::pin!(ctrl_c);
+
 	// Read events from config
 	let config_path = {
 		const ENV: &str = "MY_TIMERS_EVENTS";
@@ -69,33 +59,72 @@ async fn main() -> Result<(), Box<dyn Error>> {
 			Err(err)
 		}).unwrap_or(DEFAULT.into())
 	};
-	let events = events::parse(&config_path, pool.clone()).await?;
+	let events = tokio::select! {
+		evts = events::parse(&config_path, pool.clone()) => evts?,
+		Ok(_) = &mut ctrl_c => return shutdown(None, pool).await
+	};
+
 
 	// Initialize task joinset
-	let mut set = JoinSet::<()>::new();
+	let mut event_threads = JoinSet::<()>::new();
 	// Timer thread (runs matching events every minute at xx:00)
 	event!(Level::DEBUG, "Starting event loop in {:?}", to_next_minute());
-	sleep(to_next_minute()).await;
+	tokio::select! {
+		_ = sleep(to_next_minute()) => {},
+		Ok(_) = &mut ctrl_c => return shutdown(Some(event_threads), pool).await
+	}
 	event!(parent: None, Level::INFO, "Ready!");
-	let mut interval = time::interval(Duration::from_secs(60));
 	// Start minute interval ticker
+	let mut interval = time::interval(Duration::from_secs(60));
+
+	// Event loop
 	loop {
-		interval.tick().await;
+		// Wait for the next minute, breaking the loop if ctrl+c is pressed
+		tokio::select! {
+			_ = interval.tick() => {},
+			Ok(_) = &mut ctrl_c => return shutdown(Some(event_threads), pool).await
+		}
 		// Iterate through each event, run the ones that match
 		let now = Local::now();
 		for evt in &events {
 			if evt.interval.match_time(&now) {
 				let pool = pool.clone();
 				let evt = evt.clone();
-				set.spawn(async move {
+				event_threads.spawn(async move {
 					// Error logging is handled in the event's tracing span
 					evt.run(pool).await.ok();
 				});	
 			}
 		}
 	}
+}
 
-	set.shutdown().await;
+fn to_next_minute() -> Duration {
+	let mut now = chrono::Utc::now();
+	// Ceil to nearest ms + 1
+	// TODO: explain timing of event loop start
+	now = now.with_nanosecond(1_000_000 * (now.nanosecond() / 1_000_000) + 2_000_000).unwrap();
+	let mut next = now.clone();
+	next = if next.minute() == 59 {
+		next.with_hour(next.hour()+1).unwrap()
+		.with_minute(0).unwrap()
+	} else {
+		next.with_minute(next.minute()+1).unwrap()
+	};
+	next = next.with_second(0).unwrap()
+		.with_nanosecond(0).unwrap();
+	(next - now).to_std().unwrap()
+}
 
+#[instrument(name = "Shutting down", skip_all, err)]
+async fn shutdown(event_threads: Option<JoinSet<()>>, pool: mysql_async::Pool) -> Result<(), Box<dyn Error>> {
+	event!(Level::INFO, "Shutting down");
+	if let Some(mut threads) = event_threads {
+		event!(Level::DEBUG, "Stopping event threads");
+		threads.shutdown().await;
+	}
+	event!(Level::DEBUG, "Disconnecting from database");
+	pool.disconnect().await?;
+	event!(Level::INFO, "Done");
 	Ok(())
 }
