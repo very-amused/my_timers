4f5e0249c62276f6e92f1f6b50219c5ad11d20f7
very-amused <very-amused@pm.me>
2023-10-23 13:18:03 -0400

    Improve parsing

--- src/args.rs
+++ src/args.rs
@@ -15,40 +15,45 @@ const EVENTS_PATH_DEFAULT: &str = "events.conf";
 
 /// Parse program arguments
 pub fn args() -> Args {
-	let cli_args: Vec<String> = env::args().collect();
-	{
-		let show_help = cli_args.iter().any(|a| a == "-h" || a == "--help");
-		if show_help {
-			const USAGE: &str = "my_timers [-c/--config /path/to/config.json] [-e/--events /path/to/events.conf] [-v/--verbose] [-h/--help]";
-			println!("{}", "Usage:".to_owned() + "\n\t" + USAGE);
-			exit(0);
-		}
-	}
-	let verbose = cli_args.iter().any(|a| a == "-v" || a == "--verbose");
-
-	// Try to get config/events paths from CLI args/env
+	// Argument values to be set
+	let mut verbose = false;
 	let mut config_path: Option<String> = None;
-	if let Some((i, _)) = cli_args.iter().enumerate()
-		.find(|a| a.1 == "-c" || a.1 == "--config") {
-		if i < cli_args.len() {
-			config_path = Some(cli_args[i+1].to_owned());
-		}
-	} else if let Ok(path) = env::var(CONFIG_PATH_ENV) {
-		config_path = Some(path);
-	} else if verbose {
-		eprintln!("{} is not set, defaulting to {}", CONFIG_PATH_ENV, CONFIG_PATH_DEFAULT);
-	}
-
 	let mut events_path: Option<String> = None;
-	if let Some((i, _)) = cli_args.iter().enumerate()
-		.find(|a| a.1 == "-e" || a.1 == "--events") {
-		if i < cli_args.len() {
-			events_path = Some(cli_args[i+1].to_owned());
+
+	let args: Vec<String> = env::args().collect();
+	for (i, arg) in args.iter().enumerate() {
+		match arg.as_str() {
+			"-h" | "--help" => {
+				const USAGE: &str = "my_timers [-c/--config /path/to/config.json] [-e/--events /path/to/events.conf] [-v/--verbose] [-h/--help]";
+				println!("{}", "Usage:".to_owned() + "\n\t" + USAGE);
+				exit(0);
+			},
+			"-v" | "--verbose" => {
+				verbose = true;
+			},
+			"-c" | "--config" if i < args.len() - 1 => {
+				config_path = Some(args[i+1].to_string());
+			},
+			"-e" | "--events" if i < args.len() - 1 => {
+				events_path = Some(args[i+1].to_string());
+			}
+			_ => {}
 		}
-	} else if let Ok(path) = env::var(EVENTS_PATH_ENV) {
-		events_path = Some(path);
-	} else if verbose {
-		eprintln!("{} is not set, defaulting to {}", EVENTS_PATH_ENV, EVENTS_PATH_DEFAULT);
+	}
+	// Set unspecified flags from env vars
+	if config_path == None {
+		if let Ok(path) = env::var(CONFIG_PATH_ENV) {
+			config_path = Some(path);
+		} else if verbose {
+			eprintln!("{} is not set, defaulting to {}", CONFIG_PATH_ENV, CONFIG_PATH_DEFAULT);
+		}
+	}
+	if events_path == None {
+		if let Ok(path) = env::var(EVENTS_PATH_ENV) {
+			events_path = Some(path);
+		} else if verbose {
+			eprintln!("{} is not set, defaulting to {}", EVENTS_PATH_ENV, EVENTS_PATH_DEFAULT);
+		}
 	}
 
 	Args {
