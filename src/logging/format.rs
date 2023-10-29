b5de772b8502b8bceef634092ebf1e3fcb749c47
very-amused <very-amused@pm.me>
2023-10-29 03:52:28 -0400

    Clean up log format handling

--- src/logging/format.rs
+++ src/logging/format.rs
@@ -1,5 +1,5 @@
 use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
-use tracing_subscriber::fmt::{FormatEvent, format, FormatFields};
+use tracing_subscriber::fmt::format;
 use tracing_subscriber::registry;
 use tracing_subscriber::prelude::*;
 
@@ -7,6 +7,7 @@ use super::{BoxedLayer, time_format};
 
 pub use format::format as fmt;
 
+// Configure logging format specifics
 macro_rules! config_fmt {
 	($fmt:expr) => {
 		$fmt
@@ -17,14 +18,24 @@ macro_rules! config_fmt {
 			.with_target(false)
 	}
 }
+
+// Create a new guarded formatting layer tuple
 macro_rules! fmt_layer {
-	($non_blocking:ident,$_guard:ident,$log_fmt:expr,$field_fmt:expr) => {
-		(Some(fmt_layer($non_blocking, config_fmt!($log_fmt), $field_fmt)), Some($_guard))
+	($writer:ident,$_guard:ident,$log_fmt:expr,$field_fmt:expr) => {
+		(Some(
+			tracing_subscriber::fmt::layer()
+				.with_writer($writer)
+				.fmt_fields($field_fmt)
+				.event_format(config_fmt!($log_fmt))
+				.boxed()
+		), Some($_guard))
 	}
 }
 
 /// Create a guarded formatting layer
-pub fn guarded_fmt_layer<S: tracing::Subscriber + for<'a> registry::LookupSpan<'a>>(writer: NonBlocking, _guard: WorkerGuard, config_format: Option<&str>) -> (Option<BoxedLayer<S>>, Option<WorkerGuard>) {
+pub fn guarded_fmt_layer<S>(writer: NonBlocking, _guard: WorkerGuard, config_format: Option<&str>) -> (Option<BoxedLayer<S>>, Option<WorkerGuard>)
+where S: tracing::Subscriber + for<'a> registry::LookupSpan<'a>
+{
 	if let Some(config_format) = config_format {
 		match config_format {
 			"pretty" => fmt_layer!(writer,_guard,fmt().pretty(),format::PrettyFields::new()),
@@ -38,19 +49,3 @@ pub fn guarded_fmt_layer<S: tracing::Subscriber + for<'a> registry::LookupSpan<'
 		fmt_layer!(writer,_guard,fmt(),format::DefaultFields::new())
 	}
 }
-
-
-/// Formatting layer using log_format as formatter
-pub fn fmt_layer<S, N, E>(writer: NonBlocking, log_fmt: E, field_fmt: N) -> BoxedLayer<S>
-where
-	S: tracing::Subscriber + for<'a> registry::LookupSpan<'a>,
-	N: for<'a> FormatFields<'a> + Send + Sync + 'static,
-	E: FormatEvent<S, N> + Send + Sync + 'static
-{
-	tracing_subscriber::fmt::layer()
-		.with_writer(writer)
-		.fmt_fields(field_fmt)
-		.event_format(log_fmt)
-		.boxed()
-}
-
